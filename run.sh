e4437390e84858ab5d7467d734514aadc7e146c8
Jeff Liaw <rampant1018@gmail.com>
2015-09-19 15:47:33 +0800

    Run test script

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+rm time_baseline.txt
+rm time_avx.txt
+rm time_leibniz.txt
+rm error_baseline.txt
+rm error_avx.txt
+rm error_leibniz.txt
+N=(1 2 4 8 16 32 64 128 256 512 1024)
+for i in {0..2}
+do
+	for n in ${N[@]}
+	do
+		./compute_pi $i $n
+	done
+done
