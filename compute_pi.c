bdb371fb46305bc9349bc7ee599174dfea7b8ded
Jeff Liaw <rampant1018@gmail.com>
2015-09-19 15:46:22 +0800

    Output data point file

--- compute_pi.c
+++ compute_pi.c
@@ -59,39 +59,55 @@ double compute_pi_leibniz(size_t dt)
 int main(int argc, char* argv[])
 {
 	unsigned int operation = atoi(argv[1]);
-	size_t dt = atoi(argv[2]);
+	size_t n = atoi(argv[2]);
 	clock_t begin, end;
 	double time_spent;
 	double (*compute_pi)(size_t);
 	char method_name[32];
+	char time_filename[32];
+	char error_filename[32];
 
 	switch(operation) {
 		case 0:
 			compute_pi = &compute_pi_baseline;
 			strcpy(method_name, "compute_pi_baseline");
+			strcpy(time_filename, "time_baseline.txt");
+			strcpy(error_filename, "error_baseline.txt");
 			break;
 		case 1:
 			compute_pi = &compute_pi_avx;
 			strcpy(method_name, "compute_pi_avx");
+			strcpy(time_filename, "time_avx.txt");
+			strcpy(error_filename, "error_avx.txt");
 			break;
 		case 2:
 			compute_pi = &compute_pi_leibniz;
 			strcpy(method_name, "compute_pi_leibniz");
+			strcpy(time_filename, "time_leibniz.txt");
+			strcpy(error_filename, "error_leibniz.txt");
 			break;
 		default:
 			break;
 	}
 
 	begin = clock();
-	double pi = compute_pi(dt);
+	double pi = compute_pi(n * 1000000);
 	end = clock();
 	time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 
 	double diff = pi - M_PI > 0 ? pi - M_PI : M_PI - pi;
 	double error = diff / M_PI;
 
-	printf("%s(%zu) needs %lf sec.\n", method_name, dt, time_spent);
+	printf("%s(%zuM) needs %lf sec.\n", method_name, n, time_spent);
 	printf("error rate = %.15lf\n", error);
+
+	FILE *fw = fopen(time_filename, "a");
+	fprintf(fw, "%zu %lf\n", n, time_spent);
+	fclose(fw);
+
+	fw = fopen(error_filename, "a");
+	fprintf(fw, "%zu %.15lf\n", n, error);
+	fclose(fw);
 
 	return 0;
 }
