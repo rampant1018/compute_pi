6c239371fc148844e8f5f6491253a941ff278d7d
Jeff Liaw <rampant1018@gmail.com>
2015-09-21 02:08:16 +0800

    Update run script and gnuplot script, run(plot) avx_opt and fma version

--- run.sh
+++ run.sh
@@ -3,15 +3,20 @@ rm time_baseline.txt
 rm time_avx.txt
 rm time_leibniz.txt
 rm time_leibniz_avx.txt
+rm time_leibniz_avx_opt.txt
+rm time_leibniz_fma.txt_
 rm error_baseline.txt
 rm error_avx.txt
 rm error_leibniz.txt
 rm error_leibniz_avx.txt
+rm error_leibniz_avx_opt.txt
+rm error_leibniz_fma.txt_
 N=(1 2 4 8 16 32 64 128 256 512 1024)
-for i in {0..3}
+Operation=(baseline avx leibniz leibniz_avx leibniz_avx_opt leibniz_fma)
+for op in ${Operation[@]}
 do
 	for n in ${N[@]}
 	do
-		./compute_pi $i $n
+		./compute_pi $op $n
 	done
 done
