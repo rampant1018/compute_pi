13d4998b2af3a4de48dd0ef05488e695831c4cdd
Jeff Liaw <rampant1018@gmail.com>
2015-09-21 10:09:03 +0800

    Fix type and missing sde64

--- run.sh
+++ run.sh
@@ -4,19 +4,19 @@ rm time_avx.txt
 rm time_leibniz.txt
 rm time_leibniz_avx.txt
 rm time_leibniz_avx_opt.txt
-rm time_leibniz_fma.txt_
+rm time_leibniz_fma.txt
 rm error_baseline.txt
 rm error_avx.txt
 rm error_leibniz.txt
 rm error_leibniz_avx.txt
 rm error_leibniz_avx_opt.txt
-rm error_leibniz_fma.txt_
+rm error_leibniz_fma.txt
 N=(1 2 4 8 16 32 64 128 256 512 1024)
 Operation=(baseline avx leibniz leibniz_avx leibniz_avx_opt leibniz_fma)
 for op in ${Operation[@]}
 do
 	for n in ${N[@]}
 	do
-		./compute_pi $op $n
+		sde64 -hsw ./compute_pi $op $n
 	done
 done
