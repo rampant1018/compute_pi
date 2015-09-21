4356dfa623091b0684971c1c3a56ed89d9a011e1
Jeff Liaw <rampant1018@gmail.com>
2015-09-21 10:24:45 +0800

    update run script and gnuplot script

--- run.sh
+++ run.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 rm time_baseline.txt
-rm time_avx.txt
+rm time_baseline_avx.txt
 rm time_leibniz.txt
 rm time_leibniz_avx.txt
 rm time_leibniz_avx_opt.txt
-rm time_leibniz_fma.txt
+rm time_leibniz_avx_opt_single.txt
 rm error_baseline.txt
-rm error_avx.txt
+rm error_baseline_avx.txt
 rm error_leibniz.txt
 rm error_leibniz_avx.txt
 rm error_leibniz_avx_opt.txt
-rm error_leibniz_fma.txt
+rm error_leibniz_avx_opt_single.txt
 N=(1 2 4 8 16 32 64 128 256 512 1024)
-Operation=(baseline avx leibniz leibniz_avx leibniz_avx_opt leibniz_fma)
+Operation=(baseline baseline_avx leibniz leibniz_avx leibniz_avx_opt leibniz_avx_opt_single)
 for op in ${Operation[@]}
 do
 	for n in ${N[@]}
 	do
-		sde64 -hsw ./compute_pi $op $n
+		sde64 -hsw -- ./compute_pi $op $n
 	done
 done
